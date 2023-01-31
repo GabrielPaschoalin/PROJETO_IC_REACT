83f39b9da110e264d8632eb311c2a32c24655176
Gabriel Paschoalin <gabrielhpdias33@gmail.com>
2023-01-31 02:44:11 -0300

    Home - avanço

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,33 @@
 import "./Home.css";
 import Header from "../../Components/Header/Header.js";
 import Footer from "../../Components/Footer/Footer";
-//import FotoTime from ""
 
 function Home() {
   return (
     <div className="Pagina">
       <Header />
-        <div>
-          Aqui terá uma imagem do time
+      <div className="ImagemFundo">
+        <div className="container">
+          <h1 className="TituloHome">SIGLA</h1>
+          <div className="Subtitle">Nome do Grupo</div>
         </div>
-        <div>
-          About the Group
+      </div>
+      <div className="Page-inferior">
+        <div className="About-us">
+          <h2 class="About-title">ABOUT US</h2>
+          <p class="About-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa
+            sem, efficitur molestie auctor a, accumsan eget libero. Sed ac
+            tortor fringilla, tempor leo vitae, consectetur lectus. Vivamus
+            condimentum scelerisque enim et iaculis. Morbi egestas nunc nec enim
+            aliquam finibus. Phasellus nec leo at libero convallis sagittis.
+            Vivamus nec nibh ut velit tristique cursus sed consequat turpis.
+            Nunc sollicitudin ligula in felis tincidunt, a aliquet arcu sodales.
+          </p>
         </div>
-        <div>
-          Where us?
-        </div>
+      </div>
+
+      <div>Where us?</div>
       <Footer />
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes as RT } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Research from "./Pages/Research/Research";
 import Team from "./Pages/Team/Team";
+import User from "./Pages/User/User";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <RT>
-        <Route element={<Home />} path="/" exact />
+        <Route element={<Home />} path="/" />
+        <Route element={<User />} path="/user" />
         <Route element={<Research />} path="/Research" />
         <Route element={<Team />} path="/Team" />
       </RT>
